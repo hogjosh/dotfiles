ae21e0472a257fb3b2e69ec51544a3cb3ac7722a
Josh Hogan <josh.hogan@me.com>
2024-08-22 11:43:30 -0400

    wezterm

---
+++ dot_wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font({
+  family = 'Hack Nerd Font Mono',
+})
+config.font_size = 15.0
+
+return config
+
